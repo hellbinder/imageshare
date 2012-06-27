4ad3ff4d056211cc5d617235fd203dbd325b2fc8
Miguel Martorell <miguel_1216@yahoo.com>
2012-06-27 18:40:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imageshare::Application.config.secret_token = '79999eca9b24a5fff690591595ef705215497eafe1f479cd46abdf7349ea65316429dfa8359e59dc99bd2963895070f4b849237a632944312b5e3b0a9edd8b16'
